bc5c76023cdda206b6d93ac025e06e4baf86645c
Geoffrey Hunter <gbmhunter@gmail.com>
2023-01-08 22:26:20 +1300

    Adding support for processing only certain years.

---
+++ README.md
@@ -0,0 +1,17 @@
+## Stats Explanation
+
+All stats are cumulative, i.e. cumulative total number at the end of year, including all years prior. Take differences between years to work out how many per year.
+
+```yaml
+- num_asciidoc_files: 0
+  num_chars: 2713807 # 
+  num_commits: 141 # Total number of commits at end of year (cumulative, not just for this year)
+  num_content_files: 925
+  num_diagrams: 0 # This is a subset of the num. of images
+  num_images: 10862
+  num_lines: 71251
+  num_markdown_files: 925
+  num_words: 379641
+  other_files: []
+  year: 2018
+  ```
--- main.py
+++ main.py
@@ -4,39 +4,56 @@ import subprocess
 from pathlib import Path
 import yaml
 
+import matplotlib.pyplot as plt
+import pandas as pd
+
 def main():
     blog_path = Path('../blog/')
     script_path = Path(os.path.dirname(os.path.realpath(__file__)))
     stats_cache_path = script_path / 'stats-cache.yaml'
+    google_analytics_path = script_path / 'google-analytics-data.yaml'
 
-    if not stats_cache_path.exists():
-        years = [            
-            2018, # 2018 is the earliest complete year we can gather stats for
-            2019,
-            2020,
-            2021,
-        ]
-
-        # cd to blog dir so git commands work
-        os.chdir(blog_path)
-
-        stats_for_all_years = []
-        for year in years:
-            stats = gather_stats(blog_path, year)
-            stats_for_all_years.append(stats)
-        print(stats_for_all_years)
-
-        # Save stats
-
-        with stats_cache_path.open('w') as file:
-            yaml.dump(stats_for_all_years, file)
+    if stats_cache_path.exists():
+        # Read in existing stats
+        print('Reading in existing stats...')
+        with stats_cache_path.open('r') as file:
+            stats_for_all_years = yaml.load(file, yaml.SafeLoader)
     else:
-        print('stats-cache.yaml exists, not re-calculating stats.')
+        stats_for_all_years = {}
+
+    # Comment-out/add years here as applicable
+    years = [
+        # 2018, # 2018 is the earliest complete year we can gather stats for
+        # 2019,
+        # 2020,
+        # 2021,
+        # 2022,
+    ]
+
+    print(f'Updating stats for the following years: {years}')
+
+    # cd to blog dir so git commands work
+    os.chdir(blog_path)
+
+    # Update stats for years listed above
+    for year in years:
+        stats = gather_stats(blog_path, year)
+        stats_for_all_years[year] = stats
+    print(stats_for_all_years)
+
+    # Save stats back to file
+    with stats_cache_path.open('w') as file:
+        yaml.dump(stats_for_all_years, file)
 
     with stats_cache_path.open() as file:
         stats_for_all_years = yaml.load(file, yaml.SafeLoader)
 
-    print(stats_for_all_years)
+    # Load Google analytics data
+    with google_analytics_path.open() as file:
+        google_analytics_data = yaml.load(file, yaml.SafeLoader)
+
+    # Create graphs
+    create_graphs(stats_for_all_years, google_analytics_data)
 
 def gather_stats(blog_path: str, year: int):
     """
@@ -44,7 +61,7 @@ def gather_stats(blog_path: str, year: int):
     """
     next_year_as_str = str(year + 1)
     cmd = f'git rev-list -1 --before="{next_year_as_str}-01-01" master'
-    completed_proc = subprocess.run(cmd, capture_output=True)    
+    completed_proc = subprocess.run(cmd, capture_output=True)
     last_commit_of_year = completed_proc.stdout.decode().strip('\n')
     if last_commit_of_year == '':
         raise RuntimeError(f'No commit exists before the start of {year}.')
@@ -66,6 +83,8 @@ def gather_stats(blog_path: str, year: int):
     num_commits = commits_one_per_line.count('\n')
     stats['num_commits'] = num_commits
 
+    # List of root paths that we will recursively scan to build up stats.
+    # Make sure page content is included in these
     root_paths_to_walk = [
         blog_path / 'content',
         blog_path / 'static' / 'images',
@@ -100,7 +119,7 @@ def gather_stats(blog_path: str, year: int):
                     stats['num_lines'] += num_lines
                 elif filename.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif', '.svg')):
                     stats['num_images'] += 1
-                elif filename.endswith('.odg'):
+                elif filename.endswith('.odg') or filename.endswith('.afdesign'):
                     stats['num_diagrams'] += 1
                 else:
                     # stats['other_files'].append(filename)
@@ -109,6 +128,9 @@ def gather_stats(blog_path: str, year: int):
     return stats
 
 def get_file_stats(file_path):
+    """
+    Run this on .md and .adoc files.
+    """
     num_words = 0
     num_chars = 0
     num_lines = 0
@@ -121,6 +143,25 @@ def get_file_stats(file_path):
             num_chars += len(line)
 
     return num_chars, num_words, num_lines
+
+def create_graphs(stats_for_all_years, google_analytics_data):
+    fig, ax = plt.subplots()
+
+    df_data = {}
+    column_names = []
+    for year in stats_for_all_years:
+        values = []
+        for (stat, value) in stats_for_all_years[year].items():
+            values.append(value)
+        df_data[year] = values
+        # Re-assigns multiple times but this ok, we also assume the column names
+        # are the same for every year
+        column_names = list(stats_for_all_years[year].keys())
+
+    df = pd.DataFrame.from_dict(df_data, orient='index', columns=column_names)
+    # df = df.append(google_analytics_data, ignore_index=True)
+    # df = df.transpose()
+    print(df)
 
 if __name__ == '__main__':
     main()
